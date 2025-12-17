03b34211059264e674bbefd570f1d17110f5454f
nvrthls7-cmyk <nvrthls7@gmail.com>
2025-12-17 16:30:18 +0530

    Update sonarBackend.groovy

--- vars/sonarBackend.groovy
+++ vars/sonarBackend.groovy
@@ -1,9 +1,16 @@
 def call() {
-    withCredentials([
-        string(credentialsId: 'sonar-token', variable: 'SONAR_TOKEN')
-    ]) {
-        sh """
-        sonar-scanner         -Dsonar.projectKey=${env.JOB_NAME}-backend         -Dsonar.projectName=${env.JOB_NAME}-backend         -Dsonar.host.url=http://sonarqube:9000         -Dsonar.java.binaries=target         -Dsonar.token=$SONAR_TOKEN
-        """
+    dir('backend') {
+        withCredentials([
+            string(credentialsId: 'sonar-token', variable: 'SONAR_TOKEN')
+        ]) {
+            sh """
+            sonar-scanner \
+            -Dsonar.projectKey=${env.JOB_NAME}-backend \
+            -Dsonar.projectName=${env.JOB_NAME}-backend \
+            -Dsonar.host.url=http://sonarqube:9000 \
+            -Dsonar.java.binaries=target \
+            -Dsonar.token=$SONAR_TOKEN
+            """
+        }
     }
 }
