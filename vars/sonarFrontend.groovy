dd44f0e97889aaae94ac63d95c23ed941830c17a
nvrthls7-cmyk <nvrthls7@gmail.com>
2025-12-17 16:44:25 +0530

    Update sonarFrontend.groovy

--- vars/sonarFrontend.groovy
+++ vars/sonarFrontend.groovy
@@ -1,9 +1,17 @@
 def call() {
-    withCredentials([
-        string(credentialsId: 'sonar-token', variable: 'SONAR_TOKEN')
-    ]) {
-        sh """
-        sonar-scanner         -Dsonar.projectKey=${env.JOB_NAME}-frontend         -Dsonar.projectName=${env.JOB_NAME}-frontend         -Dsonar.host.url=http://sonarqube:9000         -Dsonar.sources=src         -Dsonar.token=$SONAR_TOKEN
-        """
+    dir('frontend') {
+        def scannerHome = tool 'sonarqube'
+        withCredentials([
+            string(credentialsId: 'sonar-token', variable: 'SONAR_TOKEN')
+        ]) {
+            sh """
+            ${scannerHome}/bin/sonar-scanner \
+            -Dsonar.projectKey=${env.JOB_NAME}-frontend \
+            -Dsonar.projectName=${env.JOB_NAME}-frontend \
+            -Dsonar.host.url=http://sonarqube:9000 \
+            -Dsonar.sources=src \
+            -Dsonar.token=$SONAR_TOKEN
+            """
+        }
     }
 }
