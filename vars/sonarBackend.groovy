23eaf1dbf7a5fffdcba1a6966ce47787c1ca4781
nvrthls7-cmyk <nvrthls7@gmail.com>
2025-12-17 16:45:54 +0530

    Update sonarBackend.groovy

--- vars/sonarBackend.groovy
+++ vars/sonarBackend.groovy
@@ -8,7 +8,7 @@ def call() {
             ${scannerHome}/bin/sonar-scanner \
             -Dsonar.projectKey=${env.JOB_NAME}-backend \
             -Dsonar.projectName=${env.JOB_NAME}-backend \
-            -Dsonar.host.url=http://sonarqube:9000 \
+            -Dsonar.host.url=http://sonarqube:9001 \
             -Dsonar.java.binaries=target \
             -Dsonar.token=$SONAR_TOKEN
             """
